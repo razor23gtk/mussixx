f022644f58525ecc6c0aa557d6874556a68d6c10
razor23gtk <razorclaw996@gmail.com>
2025-03-13 01:56:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "apollo belvedre", 
+  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
       name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      password: "0682240100razor",
+      host: "78.46.65.243",
+      port:  5643,
       secure: false
     }
   ]
